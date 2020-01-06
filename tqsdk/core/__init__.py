09d8149f3268929201a02aed3f88954ae3cb65ac
DESKTOP-ONIILQK\58470 <lijiayao@tongyu.tech>
2020-01-06 09:34:49 +0800

    初次提交，

---
+++ tqsdk/core/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*- 
+# author: limm_666
+
+from core import series_tools
---
+++ tqsdk/core/kLine.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*- 
+# author: limm_666
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+y = np.zeros((5,), dtype=np.int)
+print(y)
