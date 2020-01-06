47a8db08c7fdb4cc5839216393a5cde14cbe3e17
DESKTOP-ONIILQK\58470 <lijiayao@tongyu.tech>
2020-01-06 13:16:40 +0800

    no message

--- tqsdk/core/main.py
+++ tqsdk/core/main.py
@@ -3,10 +3,28 @@
 
 
 from core.quantitative_analysis import series_tools as tools
-tool = tools.AnalysisTools()
+from core.download import download as dl
+from datetime import datetime, date
+from tqsdk import TqApi
 
+tool = tools.AnalysisTools()
+dataDownload = dl.DataDownload()
+api = TqApi(web_gui=True)
+klines = api.get_kline_serial("DCE.y2005", 60 * 60 * 24, 50)
 
 try:
-    tool.spreadAnalysis("DCE.c1809", "DCE.c1805")
+
+    # 　根据k线进行分析
+    tool.processKline(klines)
+    #  下载数据
+    # dataDownload.download("y1805", "DCE.y1805", 60 * 60 * 24, date(2017, 7, 1), date(2018, 7, 1),
+    #                       "./download/DCE.y1805.csv")
+    #  分析价差
+    # tool.spreadAnalysis("DCE.c1809", "DCE.c1805")
 except FileNotFoundError as e:
     print(e)
+except Exception as e:
+    print(e)
+
+while True:
+    api.wait_update()
--- tqsdk/core/quantitative_analysis/series_tools.py
+++ tqsdk/core/quantitative_analysis/series_tools.py
@@ -2,6 +2,7 @@
 # author: limm_666
 import pandas as pd
 from matplotlib import pyplot as plt
+from tqsdk import tafunc
 import numpy as np
 import sys
 
@@ -34,7 +35,7 @@ class AnalysisTools(object):
         ForwardCode_date = pd.Series(ForwardCode_close.values, index=ForwardCode_date)
         spread = nearByCode_close - ForwardCode_date
         clear_spread = spread.dropna(axis=0, how='any')
-        
+
         # clear_spread = pd.Series()
         # for i, v in spread.items():
         #     if not np.isnan(v):
@@ -47,4 +48,48 @@ class AnalysisTools(object):
         plt.figure(figsize=(20, 8), dpi=80)
         plt.title('avg spread%f' % avg_spread)
         plt.plot(x, y)
+        plt.show()
+
+    # 成交，持仓分析
+    def processKline(self, kline):
+        # 成交量
+        trade_volume = kline["volume"]
+        # 增仓量
+        net_incr_volume = kline["close_oi"] - kline["open_oi"]
+        # 结算价
+        close = kline["close"]
+        # 日期
+        datetime = kline["datetime"]
+
+        data = {
+            'datetime': datetime,
+            'trade_volume': trade_volume,
+            'net_incr_volume': net_incr_volume,
+            'close': close,
+        }
+        staticData = pd.DataFrame(data)
+        staticData.to_csv("./download/staticData.csv")
+
+        for index, row in staticData.iterrows():
+            staticData.loc[index, 'datetime'] = tafunc.time_to_datetime(row['datetime'])
+
+        x = staticData['datetime']
+        y1 = staticData['trade_volume']
+        y2 = staticData['net_incr_volume']
+        y3 = staticData['close']
+
+        # 设置图形大小
+        plt.figure(figsize=(20, 8), dpi=80)
+        plt.subplot(311)
+        plt.ylabel("trade_volume")
+        plt.plot(x, y1)
+
+        plt.subplot(312)
+        plt.ylabel("net_incr_volume")
+        plt.plot(x, y2)
+
+        plt.subplot(313)
+        plt.ylabel("close")
+        plt.plot(x, y3)
+
         plt.show()
