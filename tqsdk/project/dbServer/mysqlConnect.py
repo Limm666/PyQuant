ea82d6d479a1f7a6dc5af7f771c52d5cf1b56c3e
DESKTOP-ONIILQK\58470 <lijiayao@tongyu.tech>
2020-01-09 21:13:55 +0800

    mysql模块

---
+++ tqsdk/project/dbServer/mysqlConnect.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*- 
+# author: limm_666
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="106.12.193.65",
+    user="root",
+    passwd="root",
+    database="test"
+)
+print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
