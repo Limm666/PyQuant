7226c6b39ff01d88f36c2fde3a0b4ab70d3c0f38
limm <584707735@qq.com>
2020-01-11 20:35:56 +0800

    logger 模块的添加

--- tqsdk/project/trade/trade.py
+++ tqsdk/project/trade/trade.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # author: limm_666
 
+import math
+
+
 class Trade(object):
-    def __init__(self, account, pwd, company, api):
+    def __init__(self, api):
         self.api = api
-        self.account = account
-        self.pwd = pwd
-        self.company = company
 
     def insertOrder(self, instrumentId, direction, offset, volume, limit_price):
         order = self.api.insert_order(symbol=instrumentId, direction=direction, offset=offset, volume=volume,
@@ -23,3 +23,10 @@ class Trade(object):
     def checkAcount(self):
         account = self.api.get_account()
         return account
+
+    # 控制仓位
+    def orderNum(self, quote, margin_rate):
+        account = self.checkAcount()
+        midPrice = (quote.bid_price1 + quote.ask_price1) / 2
+        volume_multiple = quote.volume_multiple
+        return math.floor(account.balance / (midPrice * volume_multiple * margin_rate * 2))
