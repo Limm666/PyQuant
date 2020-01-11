2c84b08537f89a4ae9f3ec95632e6d9528773bbb
DESKTOP-ONIILQK\58470 <lijiayao@tongyu.tech>
2020-01-11 20:33:45 +0800

    dbServer commmit

--- tqsdk/project/trade/trade.py
+++ tqsdk/project/trade/trade.py
@@ -2,11 +2,8 @@
 # author: limm_666
 
 class Trade(object):
-    def __init__(self, account, pwd, company, api):
+    def __init__(self, api):
         self.api = api
-        self.account = account
-        self.pwd = pwd
-        self.company = company
 
     def insertOrder(self, instrumentId, direction, offset, volume, limit_price):
         order = self.api.insert_order(symbol=instrumentId, direction=direction, offset=offset, volume=volume,
