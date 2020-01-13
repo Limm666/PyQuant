f4828e78c9fe22a308b378be7802f1c28d0c6c78
limm <584707735@qq.com>
2020-01-13 23:29:13 +0800

    完善macd

---
+++ tqsdk/project/MACDmain.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*- 
+# author: limm_666
+import sys
+# sys.path.append("../")
+
+from project.trade.trade import Trade
+from project.trade.quantitive_trade import QuantTrade
+from tqsdk import TqApi, TqAccount, TqSim
+
+api = TqApi(TqSim(init_balance=10000.0), web_gui="http://127.0.0.1:10001")
+klines = api.get_kline_serial("DCE.y2005", 60)
+quote = api.get_quote("DCE.y2005")
+trade = Trade(api)
+account = trade.checkAcount()
+qt = QuantTrade(api, trade, "DCE.y2005")
+
+while True:
+    api.wait_update()
+    account = trade.checkAcount()
+    if api.is_changing(klines):
+        qt.macd_trade(klines, quote)
